db22c66a12e2e4e8d2d6ad9afae87ac400610b2d
Ivan Szebenszki <szib77@gmail.com>
2019-03-02 09:32:23 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
 //define functions here
+function getIt() {
+  window.alert('Hey!')
+}
+
+function frameIt() {
+  $('img').addClass('tasty')
+}
+
+function pressIt() {
+    window.alert('G was pressed.')
+}
+
+function submitIt() {
+  window.alert('Your form is going to be submitted now.')
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  $('p').on('click', getIt)
+  $('img').on('load', frameIt)
+  $('#typing').on('keydown', function(keyEvent) {
+    if (keyEvent.keyCode === 71) {
+      pressIt()
+    }
+  })
+  $('form').on('submit', submitIt)
 });
+
+
