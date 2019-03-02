8e1b5ee7865cd30f3fca41ec86a5d9c4535fa673
Ivan Szebenszki <szib77@gmail.com>
2019-03-02 09:41:59 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -6,28 +6,32 @@ function getIt() {
 }
 
 function frameIt() {
-  $('img').addClass('tasty')
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
 }
 
 function pressIt() {
-    window.alert('G was pressed.')
+  $('#typing').on('keydown', function(keyEvent) {
+    if (keyEvent.keyCode === 71) {
+      window.alert('G was pressed.')
+    }
+  })
 }
 
 function submitIt() {
-  window.alert('Your form is going to be submitted now.')
+  $('form').on('submit', function() {
+    window.alert('Your form is going to be submitted now.')
+  })
 }
 
 $(document).ready(function(){
 
 // call functions here
-  $('p').on('click', getIt)
-  $('img').on('load', frameIt)
-  $('#typing').on('keydown', function(keyEvent) {
-    if (keyEvent.keyCode === 71) {
-      pressIt()
-    }
-  })
-  $('form').on('submit', submitIt)
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
 
 
